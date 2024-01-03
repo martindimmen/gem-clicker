ea30ea40ac45a8125e03004d295ce37473725d03
martindimmen <martindimmen@proton.me>
2024-01-03 22:16:30 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import ClickArea from "./components/ClickArea";
+function App() {
+  return (
+    <div className="App">
+      <ClickArea />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ClickArea.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ClickArea() {
+  return;
+}
+
+export default ClickArea;
